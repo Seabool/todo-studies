1c7adff59c63690c55de548492351f065f3eca55
Sebastian Stachecki <xenophobiouspl@gmail.com>
2021-01-31 16:04:50 +0100

    refactor!: use to make class objects

---
+++ src/seabool/StudentClass.java
@@ -0,0 +1,60 @@
+package seabool;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class StudentClass implements Comparable<StudentClass>{
+
+    private String className;
+    private File classDirectory;
+    private final ArrayList<String> classNotes = new ArrayList<>();
+
+    public StudentClass(String className, File classDirectory){
+        this.className = className;
+        this.classDirectory = classDirectory;
+    }
+
+    public StudentClass(String className){
+        this.className = className;
+    }
+
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public File getClassDirectory() {
+        return classDirectory;
+    }
+
+    public void setClassDirectory(File classDirectory) {
+        this.classDirectory = classDirectory;
+    }
+
+    public void addNote(String note){
+        classNotes.add(note);
+    }
+
+    public ArrayList<String> getNotes(){
+        return classNotes;
+    }
+
+    @Override
+    public int compareTo(StudentClass o) {
+        return className.compareTo(o.getClassName());
+    }
+
+    @Override
+    public int hashCode() {
+        return className.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return className.equals(((StudentClass)obj).className);
+    }
+}
